0c99637c0985c415be45448b802c5c57629fc588
tourman <8002615@gmail.com>
2014-09-02 02:26:17 +0300

    #1: SleepTest.php has been added

---
+++ tests/FiniteStateMachine/SleepTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(dirname(__FILE__) . implode(DIRECTORY_SEPARATOR, explode('/', '/../FsmTestCase.php')));
+
+/**
+ * public function test_Sleep_Before_MethodIsAllowed()
+ * public function test_Sleep_After_MethodIsBlocked()
+ * public function test_Sleep_Default_ReturnsLog()
+ * public function test_Sleep_Default_AppendsSleepItemToLog()()
+ */
+class Fsm_ActionTest extends FsmTestCase
+{
+}
