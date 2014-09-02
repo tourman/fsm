e5459e7c09a884e355dd545c3761344a4369a115
tourman <8002615@gmail.com>
2014-09-02 22:58:33 +0300

    #1: Fsm_SleepTest::test_Sleep_Before_MethodIsAllowed()

--- tests/FiniteStateMachine/SleepTest.php
+++ tests/FiniteStateMachine/SleepTest.php
@@ -8,6 +8,41 @@ require_once(dirname(__FILE__) . implode(DIRECTORY_SEPARATOR, explode('/', '/../
  * public function test_Sleep_Default_ReturnsLog()
  * public function test_Sleep_Default_AppendsSleepItemToLog()()
  */
-class Fsm_ActionTest extends FsmTestCase
+class Fsm_SleepTest extends FsmTestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        $stateSet = array_shift(array_shift($this->provideValidStateSets()));
+        $this->_fsm->setStateSet($stateSet);
+    }
+
+    public function provideMethods()
+    {
+        return array(
+            array(
+                'method' => 'action',
+                'arguments' => array('checkout'),
+            ),
+            array(
+                'method' => 'reset',
+                'arguments' => array(),
+            ),
+            array(
+                'method' => 'sleep',
+                'arguments' => array(),
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider provideMethods
+     * @expectedException Exception
+     * @expectedExceptionMessage 47fcddc8193f1bed347ae752d8b30bbe
+     */
+    public function test_Sleep_Before_MethodIsAllowed($method, $arguments)
+    {
+        call_user_func_array(array($this->_fsm, $method), $arguments);
+        throw new Exception('47fcddc8193f1bed347ae752d8b30bbe');
+    }
 }
