0a4a6215441f23619013d59dc0035d4d36ee3c39
tourman <8002615@gmail.com>
2014-10-08 19:30:08 +0300

    #1: Fsm_VerifyLog_Reason_InitTest has been deleted

--- tests/FiniteStateMachine/VerifyLog/ReasonTest.php
+++ tests/FiniteStateMachine/VerifyLog/ReasonTest.php
@@ -1,13 +1,15 @@
 <?php
 
 /**
- * public function test_VerifyLog_Reason_Init_MiddlePosition_ThrowsException
- * public function test_VerifyLog_Reason_Init_LastPosition_ThrowsException
- * public function test_VerifyLog_Reason_Action_FirstPosition_ThrowsException
- * public function test_VerifyLog_Reason_Action_LastPosition_ThrowsException
- * public function test_VerifyLog_Reason_Reset_FirstPosition_ThrowsException
- * public function test_VerifyLog_Reason_Reset_LastPosition_ThrowsException
- * public function test_VerifyLog_Reason_Sleep_FirstPosition_ThrowsException
+ * public function test_VerifyLog_Reason_TheFirstPosition_NotInit_ThrowsException
+ * public function test_VerifyLog_Reason_TheLastPosition_NotSleep_ThrowsException
+ * public function test_VerifyLog_Reason_Init_NotAtTheFirstPosition_ThrowsException
+ * public function test_VerifyLog_Reason_Action_AtTheFirstPosition_ThrowsException
+ * public function test_VerifyLog_Reason_Action_AtTheLastPosition_ThrowsException
+ * public function test_VerifyLog_Reason_Reset_AtTheFirstPosition_ThrowsException
+ * public function test_VerifyLog_Reason_Reset_AtTheLastPosition_ThrowsException
+ * public function test_VerifyLog_Reason_Wakeup_NotAfterSleep_ThrowsException
+ * public function test_VerifyLog_Reason_Sleep_NotAtTheLastPosition_NotBeforeWakeup_ThrowsException
  */
 class Fsm_VerifyLog_ReasonTest extends Fsm_VerifyLogTestCase
 {
