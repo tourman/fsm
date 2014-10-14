da4a9e9cfd7f2dfafb1c59b494229d0a32eec3d0
tourman <8002615@gmail.com>
2014-10-14 05:46:34 +0300

    #1: Fsm_VerifyLog_StateTest

---
+++ tests/FiniteStateMachine/VerifyLog/SymbolTest.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once(dirname(__FILE__) . implode(DIRECTORY_SEPARATOR, explode('/', '/../VerifyLogTestCase.php')));
+
+/**
+ * public function test_VerifyLog_InitReasonWithNotEmptySymbol_ThrowsException
+ * public function test_VerifyLog_ResetReasonWithNotEmptySymbol_ThrowsException
+ * public function test_VerifyLog_ActionReasonWithEmptySymbol_ThrowsException
+ * public function test_VerifyLog_ActionReasonWithMismatchSymbol_ThrowsException
+ * public function test_VerifyLog_SleepReasonWithNotEmptySymbol_ThrowsException
+ * public function test_VerifyLog_SleepReasonWithNotEmptySymbol_ThrowsException
+ */
+class Fsm_VerifyLog_StateTest extends Fsm_VerifyLogTestCase
+{
+}
