ff3a41c790d83ef75c49add4b03ef2c21fc6cbcd
tourman <8002615@gmail.com>
2014-10-14 15:16:32 +0300

    Fsm_DisplayLogTest::provideValidLogs() has been updated in order to be ensure to display long symbols and short states

--- tests/FiniteStateMachine/DisplayLogTest.php
+++ tests/FiniteStateMachine/DisplayLogTest.php
@@ -9,12 +9,21 @@ class Fsm_DisplayLogTest extends FsmTestCase
 {
     public function provideValidLogs()
     {
-        $expectedContent = <<<EOC
+        $expectedContent[0] = <<<EOC
 state                             reason  symbol  timestamp          diff      
 -------------------------------------------------------------------------------
 INIT                              init            1409297603.783227            
 b2118a9a6b91e1b40770231419b07f60  action  *       1409297603.918776  0.135549  
 INIT                              reset           1409297603.918778  0.000002  
+INIT                              sleep           1409297603.918779  0.000001  
+INIT                              wakeup          1409297603.918779  0.000000  
+
+EOC;
+        $expectedContent[1] = <<<EOC
+state  reason  symbol          timestamp          diff      
+------------------------------------------------------------
+INIT   init                    1409297603.783227            
+S      action  someLongSymbol  1409297603.918776  0.135549  
 
 EOC;
         return array(
@@ -38,13 +47,44 @@ EOC;
                         'symbol' => null,
                         'timestamp' => '1409297603.918778',
                     ),
+                    array(
+                        'state' => 'INIT',
+                        'reason' => 'sleep',
+                        'symbol' => null,
+                        'timestamp' => '1409297603.918779',
+                    ),
+                    array(
+                        'state' => 'INIT',
+                        'reason' => 'wakeup',
+                        'symbol' => null,
+                        'timestamp' => '1409297603.918779',
+                    ),
                 ),
-                'expectedContent' => $expectedContent,
+                'expectedContent' => $expectedContent[0],
+            ),
+            array(
+                'log' => array(
+                    array(
+                        'state' => 'INIT',
+                        'reason' => 'init',
+                        'symbol' => null,
+                        'timestamp' => '1409297603.783227',
+                    ),
+                    array(
+                        'state' => 'S',
+                        'reason' => 'action',
+                        'symbol' => 'someLongSymbol',
+                        'timestamp' => '1409297603.918776',
+                    ),
+                ),
+                'expectedContent' => $expectedContent[1],
             ),
         );
     }
 
     /**
+     * @group issue1
+     * @group issue1_log_display
      * @dataProvider provideValidLogs
      */
     public function test_DisplayLog_Default_DisplaysProperFormat($log, $expectedContent)
