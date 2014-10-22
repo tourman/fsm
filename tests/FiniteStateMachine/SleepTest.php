b683c4b54f8c56b8772a9f7d7335d7f7ffad8af9
tourman <8002615@gmail.com>
2014-10-22 17:02:12 +0300

    #1: Fsm_SleepTest has been cleaned out from obsolete methods

--- tests/FiniteStateMachine/SleepTest.php
+++ tests/FiniteStateMachine/SleepTest.php
@@ -3,11 +3,6 @@
 require_once(dirname(__FILE__) . implode(DIRECTORY_SEPARATOR, explode('/', '/../FsmTestCase.php')));
 
 /**
- * public function test_Sleep_Before_MethodIsAllowed()
- * public function test_Sleep_After_MethodIsBlocked()
- * public function test_Sleep_Default_AppendsSleepItemToLog()()
- */
-/**
  * public function test_Sleep_CallsIsSleep()
  * public function test_Sleep_SetsSleep()
  * public function test_Sleep_AppendsSleepItemToLog()
@@ -53,76 +48,6 @@ class Fsm_SleepTest extends FsmTestCase
         $sleepAfter = $this->getSleep();
         $this->assertFalse($sleepBefore);
         $this->assertTrue($sleepAfter);
-    }
-
-    public function provideMethods()
-    {
-        return array(
-            array(
-                'method' => 'action',
-                'arguments' => array('checkout'),
-            ),
-            array(
-                'method' => 'reset',
-                'arguments' => array(),
-            ),
-            array(
-                'method' => 'sleep',
-                'arguments' => array(),
-            ),
-        );
-    }
-
-    /**
-     * @group issue1
-     * @group issue1_log_sleep
-     * @dataProvider provideMethods
-     * @expectedException Exception
-     * @expectedExceptionMessage 47fcddc8193f1bed347ae752d8b30bbe
-     */
-    public function test_Sleep_Before_MethodIsAllowed($method, $arguments)
-    {
-        call_user_func_array(array($this->_fsm, $method), $arguments);
-        throw new Exception('47fcddc8193f1bed347ae752d8b30bbe');
-    }
-
-    /**
-     * @group issue1
-     * @group issue1_log_sleep
-     * @dataProvider provideMethods
-     * @expectedException RuntimeException
-     * @expectedExceptionCode 112
-     * @expectedExceptionMessage Could not call method over the sleep mode
-     */
-    public function test_Sleep_After_MethodIsBlocked($method, $arguments)
-    {
-        $this->_fsm->sleep();
-        call_user_func_array(array($this->_fsm, $method), $arguments);
-    }
-
-    /**
-     * @group issue1
-     * @group issue1_log_sleep
-     */
-    public function test_Sleep_Default_AppendsSleepItemToLog()
-    {
-        $expectedTimestamp = md5(uniqid());
-        $expectedLog = array(
-            md5(uniqid()),
-            array(
-                'state' => null,
-                'reason' => 'sleep',
-                'symbol' => null,
-                'timestamp' => $expectedTimestamp,
-            ),
-        );
-        $log = array_slice($expectedLog, 0, -1);
-        $className = get_class($this->_fsm);
-        $this->_fsm = $this->getMockBuilder($className)->setMethods(array('getTimestamp'))->getMock();
-        $this->_fsm->expects($this->once())->method('getTimestamp')->will($this->returnValue($expectedTimestamp));
-        $this->setLog($log);
-        $log = $this->_fsm->sleep();
-        $this->assertSame($expectedLog, $log);
     }
 
     public function provideSleepLogs()
