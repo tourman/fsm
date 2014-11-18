65ae460170fa37f59e004df0a3e65176f262b924
tourman <8002615@gmail.com>
2014-11-19 01:02:11 +0200

    #22: Fsm_VerifyLog_ReasonTest::test_VerifyLog_Reason_TheFirstPosition_NotInit_ThrowsException() has been refactored

--- tests/FiniteStateMachine/VerifyLog/ReasonTest.php
+++ tests/FiniteStateMachine/VerifyLog/ReasonTest.php
@@ -8,6 +8,7 @@ require_once(dirname(__FILE__) . implode(DIRECTORY_SEPARATOR, explode('/', '/../
  * public function test_VerifyLog_Reason_InvalidValue_ThrowsException
  * public function test_VerifyLog_Reason_InvalidValue_ThrowsException_CertainKeys
  * public function test_VerifyLog_Reason_TheFirstPosition_NotInit_ThrowsException
+ * public function test_VerifyLog_Reason_TheFirstPosition_NotInit_ThrowsException_CertainKeys
  * public function test_VerifyLog_Reason_TheLastPosition_NotSleep_ThrowsException
  * public function test_VerifyLog_Reason_Init_NotAtTheFirstPosition_ThrowsException
  * public function test_VerifyLog_Reason_NotWakeup_AfterSleep_ThrowsException
@@ -298,13 +299,24 @@ class Fsm_VerifyLog_ReasonTest extends Fsm_VerifyLogTestCase
     /**
      * @group issue1
      * @group issue1_reason
+     * @group issue22
      * @dataProvider provideLogsWithNotInitFirstPosition
      * @expectedException InvalidArgumentException
      * @expectedExceptionCode 501
+     * @expectedExceptionMessageRegExp /^Argument \$log has invalid value: invalid value reason in sequence at index \d+$/
      */
     public function test_VerifyLog_Reason_TheFirstPosition_NotInit_ThrowsException($stateSet, $log, $logRecordIndex)
     {
-        $this->_testLogValue($stateSet, $log, $logRecordIndex, true);
+        $this->_fsm->verifyLog($stateSet, $log);
+    }
+
+    /**
+     * @group issue22
+     * @dataProvider provideLogsWithNotInitFirstPosition
+     */
+    public function test_VerifyLog_Reason_TheFirstPosition_NotInit_ThrowsException_CertainKeys($stateSet, $log, $logRecordIndex)
+    {
+        $this->assertExceptionMessage($stateSet, $log, 'index', $logRecordIndex);
     }
 
     public function provideLogsWithNotSleepLastPosition()
