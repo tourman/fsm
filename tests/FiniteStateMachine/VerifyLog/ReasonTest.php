2f933e2cb710f7d381fc7eccc19638d7de012a06
tourman <8002615@gmail.com>
2014-11-19 06:53:22 +0200

    #22: Fsm_VerifyLogTestCase has been updated

--- tests/FiniteStateMachine/VerifyLog/ReasonTest.php
+++ tests/FiniteStateMachine/VerifyLog/ReasonTest.php
@@ -18,28 +18,6 @@ require_once(dirname(__FILE__) . implode(DIRECTORY_SEPARATOR, explode('/', '/../
  */
 class Fsm_VerifyLog_ReasonTest extends Fsm_VerifyLogTestCase
 {
-    protected $_exceptionMessage;
-
-    public function setUp()
-    {
-        parent::setUp();
-        $this->_exceptionMessage = null;
-    }
-
-    public function assertExceptionMessage($stateSet, $log, $key, $value)
-    {
-        if (is_null($this->_exceptionMessage)) {
-            try {
-                $this->_fsm->verifyLog($stateSet, $log);
-                $this->_exceptionMessage = '';
-            } catch (Exception $e) {
-                $this->_exceptionMessage = $e->getMessage();
-            }
-        }
-        $regExp = preg_quote("$key $value", '/');
-        $this->assertRegExp("/$regExp/", $this->_exceptionMessage);
-    }
-
     public function provideLogsWithInvalidTypeReason()
     {
         $stateSet = $this->_getBillingStateSet();
--- tests/FiniteStateMachine/VerifyLog/StateTest.php
+++ tests/FiniteStateMachine/VerifyLog/StateTest.php
@@ -20,28 +20,6 @@ require_once(dirname(__FILE__) . implode(DIRECTORY_SEPARATOR, explode('/', '/../
  */
 class Fsm_VerifyLog_StateTest extends Fsm_VerifyLogTestCase
 {
-    protected $_exceptionMessage;
-
-    public function setUp()
-    {
-        parent::setUp();
-        $this->_exceptionMessage = null;
-    }
-
-    public function assertExceptionMessage($stateSet, $log, $key, $value)
-    {
-        if (is_null($this->_exceptionMessage)) {
-            try {
-                $this->_fsm->verifyLog($stateSet, $log);
-                $this->_exceptionMessage = '';
-            } catch (Exception $e) {
-                $this->_exceptionMessage = $e->getMessage();
-            }
-        }
-        $regExp = preg_quote("$key $value", '/');
-        $this->assertRegExp("/$regExp/", $this->_exceptionMessage);
-    }
-
     public function provideLogsWithInvalidTypeState()
     {
         $stateSet = $this->_getBillingStateSet();
