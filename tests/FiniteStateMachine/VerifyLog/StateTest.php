11a2aac70ebe3bf5e32a4e3b3842a3e68ce50e89
tourman <8002615@gmail.com>
2014-11-19 04:19:50 +0200

    #22: Fsm_VerifyLogTest::test_VerifyLog_ActionSleepWithStateFromNoPreviousRecord_ThrowsException() has been refactored

--- tests/FiniteStateMachine/VerifyLog/StateTest.php
+++ tests/FiniteStateMachine/VerifyLog/StateTest.php
@@ -14,6 +14,7 @@ require_once(dirname(__FILE__) . implode(DIRECTORY_SEPARATOR, explode('/', '/../
  * public function test_VerifyLog_ActionReasonWithMismatchedState_ThrowsException
  * public function test_VerifyLog_ActionReasonWithMismatchedState_ThrowsException_CertainKeys
  * public function test_VerifyLog_ActionSleepWithStateFromNoPreviousRecord_ThrowsException
+ * public function test_VerifyLog_ActionSleepWithStateFromNoPreviousRecord_ThrowsException_CertainKeys
  * public function test_VerifyLog_ActionWakeupWithStateFromNoPreviousRecord_ThrowsException
  */
 class Fsm_VerifyLog_StateTest extends Fsm_VerifyLogTestCase
@@ -520,13 +521,24 @@ class Fsm_VerifyLog_StateTest extends Fsm_VerifyLogTestCase
     /**
      * @group issue1
      * @group issue1_state
+     * @group issue22
      * @dataProvider provideLogsWithSleepReasonWithStateFromNoPreviousRecord
      * @expectedException InvalidArgumentException
      * @expectedExceptionCode 604
+     * @expectedExceptionMessageRegExp /^Argument \$log has invalid value: invalid value state in sequence at index \d+$/
      */
     public function test_VerifyLog_ActionSleepWithStateFromNoPreviousRecord_ThrowsException($stateSet, $log, $logRecordIndex)
     {
-        $this->_testLogValue($stateSet, $log, $logRecordIndex, true);
+        $this->_fsm->verifyLog($stateSet, $log);
+    }
+
+    /**
+     * @group issue22
+     * @dataProvider provideLogsWithSleepReasonWithStateFromNoPreviousRecord
+     */
+    public function test_VerifyLog_ActionSleepWithStateFromNoPreviousRecord_ThrowsException_CertainKeys($stateSet, $log, $logRecordIndex)
+    {
+        $this->assertExceptionMessage($stateSet, $log, 'index', $logRecordIndex);
     }
 
     public function provideLogsWithWakeupReasonWithStateFromNoPreviousRecord()
