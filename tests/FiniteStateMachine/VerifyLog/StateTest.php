7d372f6291d784322c8f23b091e36d576169e320
tourman <8002615@gmail.com>
2014-11-19 03:33:31 +0200

    #22: Fsm_VerifyLog_StateTest::_testLogType() and Fsm_VerifyLog_StateTest::_testLogValue() have been eliminated

--- tests/FiniteStateMachine/VerifyLog/StateTest.php
+++ tests/FiniteStateMachine/VerifyLog/StateTest.php
@@ -13,30 +13,6 @@ require_once(dirname(__FILE__) . implode(DIRECTORY_SEPARATOR, explode('/', '/../
  */
 class Fsm_VerifyLog_StateTest extends Fsm_VerifyLogTestCase
 {
-    protected function _testLogType($stateSet, $log, $logRecordIndex, $sequence)
-    {
-        $sequence = $sequence ? 'in sequence ' : '';
-        try {
-            $this->_fsm->verifyLog($stateSet, $log);
-        } catch (InvalidArgumentException $e) {
-            $this->assertInvalidTypeArgumentExceptionMessage($e, 'log');
-            $this->assertStringEndsWith("invalid type state {$sequence}at index $logRecordIndex", $e->getMessage());
-            throw $e;
-        }
-    }
-
-    protected function _testLogValue($stateSet, $log, $logRecordIndex, $sequence)
-    {
-        $sequence = $sequence ? 'in sequence ' : '';
-        try {
-            $this->_fsm->verifyLog($stateSet, $log);
-        } catch (InvalidArgumentException $e) {
-            $this->assertInvalidValueArgumentExceptionMessage($e, 'log');
-            $this->assertStringEndsWith("invalid value state {$sequence}at index $logRecordIndex", $e->getMessage());
-            throw $e;
-        }
-    }
-
     public function provideLogsWithInvalidTypeState()
     {
         $stateSet = $this->_getBillingStateSet();
