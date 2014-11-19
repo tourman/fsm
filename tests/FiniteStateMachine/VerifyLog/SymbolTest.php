56ffcaa18227cca8ffd1fc59686a9f96bbb32a01
tourman <8002615@gmail.com>
2014-11-19 06:15:48 +0200

    #22: Fsm_VerifyLog_SymbolTest::test_VerifyLog_ResetReasonWithNotEmptySymbol_ThrowsException() has been refactored

--- tests/FiniteStateMachine/VerifyLog/SymbolTest.php
+++ tests/FiniteStateMachine/VerifyLog/SymbolTest.php
@@ -10,6 +10,7 @@ require_once(dirname(__FILE__) . implode(DIRECTORY_SEPARATOR, explode('/', '/../
  * public function test_VerifyLog_InitReasonWithNotEmptySymbol_ThrowsException
  * public function test_VerifyLog_InitReasonWithNotEmptySymbol_ThrowsException_CertainKeys
  * public function test_VerifyLog_ResetReasonWithNotEmptySymbol_ThrowsException
+ * public function test_VerifyLog_ResetReasonWithNotEmptySymbol_ThrowsException_CertainKeys
  * public function test_VerifyLog_ActionReasonWithEmptySymbol_ThrowsException
  * public function test_VerifyLog_ActionReasonWithAbsentSymbol_ThrowsException
  * public function test_VerifyLog_ActionReasonWithMismatchSymbol_ThrowsException
@@ -280,13 +281,24 @@ class Fsm_VerifyLog_SymbolTest extends Fsm_VerifyLogTestCase
     /**
      * @group issue1
      * @group issue1_symbol
+     * @group issue22
      * @dataProvider provideLogsWithResetReasonWithNotEmptySymbol
      * @expectedException InvalidArgumentException
      * @expectedExceptionCode 702
+     * @expectedExceptionMessageRegExp /^Argument \$log has invalid value: invalid value symbol in sequence at index \d+$/
      */
     public function test_VerifyLog_ResetReasonWithNotEmptySymbol_ThrowsException($stateSet, $log, $logRecordIndex)
     {
-        $this->_testLogValue($stateSet, $log, $logRecordIndex, true);
+        $this->_fsm->verifyLog($stateSet, $log);
+    }
+
+    /**
+     * @group issue22
+     * @dataProvider provideLogsWithResetReasonWithNotEmptySymbol
+     */
+    public function test_VerifyLog_ResetReasonWithNotEmptySymbol_ThrowsException_CertainKeys($stateSet, $log, $logRecordIndex)
+    {
+        $this->assertExceptionMessage($stateSet, $log, 'index', $logRecordIndex);
     }
 
     public function provideLogsWithActionReasonWithEmptySymbol()
